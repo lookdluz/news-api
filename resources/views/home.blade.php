9ddf9e39ce300166e41053af7cdbae79212940b5
Lucas <dev.lukedluz@gmail.com>
2025-10-21 02:56:20 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>API de Notícias — Projeto de Portfólio</title>
+    <title>Notícias de Tecnologia — Projeto de Portfólio</title>
 
     {{-- Tailwind via CDN --}}
     <script src="https://cdn.tailwindcss.com"></script>
