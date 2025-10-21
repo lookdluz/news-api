f7a89ae750308233e1bde0851e6e92d952eb550c
Lucas <dev.lukedluz@gmail.com>
2025-10-21 02:55:21 -0300

    Update api.php (1.1)

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\NewsController;
-
-Route::get('/news', [NewsController::class, 'index']);
-Route::get('/news/{news}', [NewsController::class, 'show'])->whereNumber('news');
